c49bb24296678de550812e6b6343d550a1968759
Mahak Mujawadiya <mahak.mujawadiya@lucentinnovation.com>
2023-04-14 17:11:34 +0530

    api created

--- arduinoapi/api/views.py
+++ arduinoapi/api/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import Config
+from .serializer import ConfigSerializer
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
+class ConfigView(viewsets.ViewSet):
+    def create(self,request):
+        serializer=ConfigSerializer(data=request.data)
+        if serializer.is_valid():
+               serializer.save()
+               return Response({"msg":"data created"},status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+    def list(self,request):
+        stu=Config.objects.all()
+        serializer =ConfigSerializer(stu,many=True)
+        return Response(serializer.data)
+    
+    def retrieve(self,request,pk=None):
+        stu=Config.objects.filter(id=pk).first()
+        if stu is None:
+             return Response({"msg":"id not exist is not exist"},status=status.HTTP_404_NOT_FOUND)
+        serializer=ConfigSerializer(stu)
+        return Response(serializer.data)
+    
+    def partial_updata(self,request,pk=None):
+            stu=Config.objects.filter(id=pk).first()
+            if stu is None:
+                    return Response({"msg":"id not exist is not exist"},status=status.HTTP_404_NOT_FOUND)
+            serializer=ConfigSerializer(stu,data=request.data,partial=True)
+            if serializer.is_valid():
+                    serializer.save()
+                    return Response({"msg":"data partailly updated"})
+               
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
--- arduinoapi/arduinoapi/urls.py
+++ arduinoapi/arduinoapi/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from api import views
+from rest_framework.routers import DefaultRouter
+
+router=DefaultRouter()
+
+router.register('config',views.ConfigView,basename="config")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("",include(router.urls)),
 ]
